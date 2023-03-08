498c401fadd25488601b8b40dc4a0d3576cfb294
aferdina <aferdina@mail.uni-mannheim.de>
2023-03-08 19:23:29 +0100

    small update

--- gumbel_exp/gumbel_exp_utils.py
+++ gumbel_exp/gumbel_exp_utils.py
@@ -3,6 +3,7 @@ import scipy.stats as stats
 import random
 import re
 import json 
+from abc import ABC, abstractmethod
 
 def create_distributions(num_rep = 3):
 
@@ -60,6 +61,46 @@ def flatten_dict(d, parent_key='', sep='_'):
         else:
             items.append((new_key, v))
     return dict(items)
+
+class BoltzmannGumbelRandomVariable(ABC):
+    """ boltzmann exploration algorithm also known as softmax bandit
+    """
+
+    def __init__(self,  n_arms, some_constant, max_steps, randomvariable_dict):
+        """initialize boltzmann algorithm with constant temperature
+
+        Args:
+            temperature (float): float describing learning rate
+            n_arms (int): number of used arms
+        """
+        
+        _dist = getattr(stats, randomvariable_dict["name"])
+        self.random_variable = _dist(
+            **randomvariable_dict["parameter"]).rvs(size=(max_steps, n_arms))
+    
+    @abstractmethod
+    def select_arm(self, *args,**kwargs):
+        """ get action from boltzmann gumbel paper
+        """
+        pass
+
+    @abstractmethod
+    def update(self, *args, **kwargs):
+        """ update the value estimators and counts based on the new observed
+         reward and played action
+
+        Args:
+            chosen_arm (int): action which was played
+            reward (float): reward of the multiarmed bandit, based on playing action `chosen_arm`
+        """
+        pass
+
+    @abstractmethod
+    def reset(self):
+        """ reset agent by resetting all required statistics
+        """
+        pass
+        
 
 if __name__=="__main__":
     print(create_distributions(num_rep = 1))
